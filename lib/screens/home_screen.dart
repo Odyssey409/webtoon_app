9ba613993e2c4ac8ae80c5cc0196a1e10ec65fcd
Odyssey409 <77610388+Odyssey409@users.noreply.github.com>
2024-10-09 23:41:09 +0900

    #6.1 AppBar
    웹툰 앱의 상단 바의 기초설계 완료.
    elevation이라는 요소로 입체감을 줄 수 있다는 것을 학습.

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text(
+          "오늘의 웹툰",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        foregroundColor: Colors.green,
+        centerTitle: true,
+        shadowColor: Colors.black,
+        backgroundColor: Colors.white,
+        elevation: 2,
+      ),
+    );
+  }
+}
