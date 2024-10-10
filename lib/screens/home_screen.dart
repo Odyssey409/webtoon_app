9cf6fb2c174e342a836f5dc1b4604bc2eed5257c
Odyssey409 <77610388+Odyssey409@users.noreply.github.com>
2024-10-11 01:55:29 +0900

    #6.7 ListView
    받아온 데이터를 ListView를 이용하여 리스트로 나열하여 그리는 법을 배움.
    그냥 ListView를 사용하면, 데이터를 한번에 받아오기 때문에, 무한 스크롤을 사용하는 곳이나 그리는 양이 많아지면 메모리가 overflow 될 수 있어서 권장되는 방법은 아니고, 이 대신 ListView.builder 혹은 ListView.separated 을 사용하여 데이터를 사용자가 보는 부분을 기준으로 분리해서 필요한 부분만 그때 받아오는 방식을 사용함.
    
    이에 대한 내용은 다음 블로그에 정리함.
    https://velog.io/@odyssey/ListView%EB%A1%9C-%EB%8D%B0%EC%9D%B4%ED%84%B0-%EB%A6%AC%EC%8A%A4%ED%8A%B8%EB%A1%9C-%EA%B7%B8%EB%A6%AC%EA%B8%B0

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,7 +5,7 @@ import 'package:webtoon_app/services/api_service.dart';
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
-  Future<List<WebtoonModel>> webtoons = ApiService.fetchWebtoonsToday();
+  final Future<List<WebtoonModel>> webtoons = ApiService.fetchWebtoonsToday();
 
   @override
   Widget build(BuildContext context) {
@@ -29,9 +29,19 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return const Text("Data is loaded");
+            return ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                var webtoon = snapshot.data![index];
+                return Text(webtoon.title);
+              },
+              separatorBuilder: (context, index) => const SizedBox(width: 20),
+            );
           }
-          return const Text("Data is loading");
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
         },
       ),
     );
