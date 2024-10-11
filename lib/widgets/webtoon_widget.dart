563ad3e991b21be86bd15a1d57d152eca1aa590e
Odyssey409 <77610388+Odyssey409@users.noreply.github.com>
2024-10-12 00:27:39 +0900

    #6.9 Detail Screen
    화면의 웹툰 객체를 클릭하면 해당 웹툰의 상세 페이지로 라우팅 시켜서 ( 그렇게 보일 뿐, 사실은 그냥 stateless widget을 띄워 보여주는 것) 해당 웹툰의 상세페이지를 보여주는 기능을 구현.
    
    그 과정에서 Tap하는 행위와 같은 이벤트들을 감지하는 GestureDetector와  Navigator.push, MaterialPageRoute(애니메이션이 ios 에뮬레이터에서만 돌아 가는 것 같아서 PageRouteBuilder로 대체) 에 대해서 배움.
    
    이 내용은 다음 두 블로그에 정리함
    https://velog.io/@odyssey/GestureDetector%EB%A1%9C-%EB%8F%99%EC%9E%91-%EA%B0%90%EC%A7%80-%ED%95%98%EB%8A%94-%EB%B2%95
    https://velog.io/@odyssey/Flutter%EC%97%90%EC%84%9C-%ED%8E%98%EC%9D%B4%EC%A7%80-%EC%A0%84%ED%99%98-%EB%B0%A9%EB%B2%95

---
+++ lib/widgets/webtoon_widget.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:webtoon_app/screens/detail_screen.dart';
+
+class Webtoon extends StatelessWidget {
+  final String title, thumb, id;
+
+  const Webtoon({
+    super.key,
+    required this.title,
+    required this.thumb,
+    required this.id,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          PageRouteBuilder(
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+              var begin = const Offset(0.0, 1.0);
+              var end = Offset.zero;
+              var curve = Curves.ease;
+              var tween = Tween(begin: begin, end: end).chain(
+                CurveTween(curve: curve),
+              );
+              return SlideTransition(
+                position: animation.drive(tween),
+                child: child,
+              );
+            },
+            pageBuilder: (context, animation, secondaryAnimation) =>
+                DetailScreen(
+              id: id,
+              title: title,
+              thumb: thumb,
+            ),
+          ),
+        );
+      },
+      child: Column(
+        children: [
+          Container(
+            width: 250,
+            clipBehavior: Clip.hardEdge,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.5),
+                  blurRadius: 15,
+                  offset: const Offset(10, 10),
+                ),
+              ],
+            ),
+            child: Image.network(thumb),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 20,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
